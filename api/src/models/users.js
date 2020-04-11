1dfc6798b483371a0882f7a871d5efd1132545d2
Finlitvin <ros.litvin@gmail.com>
2020-04-11 16:06:39 +0300

    add password hash

--- api/src/models/users.js
+++ api/src/models/users.js
@@ -1,7 +1,9 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../db/sequelize');
+const hash = require('../helpers/hash');
+const bcrypt = require('bcrypt');
 
-const Users = (module.exports = sequelize.define('user', {
+const User = (module.exports = sequelize.define('user', {
     email: {
         type: Sequelize.STRING,
         allowNull: false,
@@ -12,3 +14,19 @@ const Users = (module.exports = sequelize.define('user', {
         allowNull: false
     }
 }));
+
+User.beforeCreate((user, options) => {
+    user.password = User.hashPassword(user.password);
+});
+  
+User.hashPassword = (password) => {
+    return hash(password);
+};
+  
+User.prototype.validatePassword = function(password) {
+    if (!password || !this.password) {
+        return false;
+    }
+  
+    return bcrypt.compareSync(password, this.password);
+};
